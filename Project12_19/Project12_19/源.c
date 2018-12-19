8a6e351275c5afed19ba2c5b9b374a7c4080b86a
宁鹏 <552432022@qq.com>
2018-12-19 22:59:00 +0800

    输入一个日期，计算其是一年之中的第几天

---
+++ Project12_19/Project12_19/源.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//输入某年某月某日，判断这一天是这一年的第几天？
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int day, month, year, sum, leap;
+	printf("\n请输入年、月、日:(格式为：年,月,日)\n");
+	scanf("%d,%d,%d", &year, &month, &day);  
+	switch (month) 
+	{
+	case 1:sum = 0; break;
+	case 2:sum = 31; break;
+	case 3:sum = 59; break;
+	case 4:sum = 90; break;
+	case 5:sum = 120; break;
+	case 6:sum = 151; break;
+	case 7:sum = 181; break;
+	case 8:sum = 212; break;
+	case 9:sum = 243; break;
+	case 10:sum = 273; break;
+	case 11:sum = 304; break;
+	case 12:sum = 334; break;
+	default:printf("data error"); break;
+	}
+	sum = sum + day; 
+	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) 
+	{
+		leap = 1;
+	}
+	else 
+	{
+		leap = 0;
+	}
+	if (leap == 1 && month>2) 
+	{ 
+		sum++;
+	}
+	printf("%d月%d日是%d年的第 %d 天！",month,day,year,sum);
+	printf("\n");
+	system("pause");
+
+	return 0;
+}
